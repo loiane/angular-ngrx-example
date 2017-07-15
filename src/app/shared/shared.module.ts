7526506b937f79e879e85936bb397219836206fa
loiane <loianeg@gmail.com>
2017-07-15 12:10:21 -0300

    added the new HttpClientModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { MdToolbarModule } from '@angular/material';
 
-const sharedModules = [CommonModule, MdToolbarModule];
+const sharedModules = [CommonModule, HttpClientModule, MdToolbarModule];
 
 @NgModule({
   imports: [CommonModule],
