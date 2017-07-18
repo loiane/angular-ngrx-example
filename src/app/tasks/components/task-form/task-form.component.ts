f6f129a3d7d591ca4340262a7dc340a6c018c5f2
loiane <loianeg@gmail.com>
2017-07-18 17:54:37 -0300

    added task form (top of the page)

--- src/app/tasks/components/task-form/task-form.component.ts
+++ src/app/tasks/components/task-form/task-form.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss']
 })
-export class TaskFormComponent implements OnInit {
+export class TaskFormComponent {
 
-  constructor() { }
+  @Output() createTask: EventEmitter<any> = new EventEmitter(false);
 
-  ngOnInit() {
+  @Input() title: string;
+
+  clear(): void {
+    this.title = '';
   }
 
+  onSubmit(): void {
+    const title: string = this.title.trim();
+    if (title.length) {
+      this.createTask.emit(title);
+    }
+    this.clear();
+  }
+
+  constructor() {
+    this.title = '';
+  }
 }
