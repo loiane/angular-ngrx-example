756ab66dbdaf91a23e95b9ef8a6e501acbdd191a
loiane <loianeg@gmail.com>
2017-07-18 17:55:47 -0300

    created tasks container

--- src/app/tasks/components/tasks/tasks.component.ts
+++ src/app/tasks/components/tasks/tasks.component.ts
@@ -1,3 +1,6 @@
+import { TaskStoreService } from './../../store/task-store.service';
+import { Task } from './../../model/task';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  tasks$: Observable<Task[]>;
+  isLoading$: Observable<boolean>;
+  error$: Observable<string>;
 
-  constructor() { }
+  constructor(private taskStoreService: TaskStoreService) {}
 
   ngOnInit() {
+    this.taskStoreService.dispatchLoadAction();
+    this.tasks$ = this.taskStoreService.getTasks();
+    this.isLoading$ = this.taskStoreService.getIsLoading();
+    this.error$ = this.taskStoreService.getError();
   }
 
+  onCreateTask(title) {
+    this.taskStoreService.dispatchCreateAction(new Task(title));
+  }
+
+  onRemoveTask(task) {
+    this.taskStoreService.dispatchRemoveAction(task);
+  }
+
+  onUpdateTask(event) {
+    this.taskStoreService.dispatchUpdateAction(event.updates);
+  }
 }
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,21 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { TaskStoreModule } from './store/task-store.module';
 import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { TasksRoutingModule } from './tasks-routing.module';
-import { TasksService } from './services/tasks.service';
+import { TaskService } from './services/task.service';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { TasksListComponent } from './components/tasks-list/tasks-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskEditDialogComponent } from './components/task-edit-dialog/task-edit-dialog.component';
 
 @NgModule({
-  imports: [TasksRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    TasksRoutingModule,
+    FormsModule,
+    SharedModule,
+    TaskStoreModule
+  ],
   declarations: [
     TasksComponent,
     TaskItemComponent,
     TasksListComponent,
-    TaskFormComponent
+    TaskFormComponent,
+    TaskEditDialogComponent
   ],
-  providers: [TasksService]
+  providers: [TaskService],
+  entryComponents: [TaskEditDialogComponent]
 })
 export class TasksModule {}
