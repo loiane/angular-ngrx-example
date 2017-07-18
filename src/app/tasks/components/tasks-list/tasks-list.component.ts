27786aaab7aed09493490a2abbfef100773334a9
loiane <loianeg@gmail.com>
2017-07-18 17:55:20 -0300

    added task list component

--- src/app/tasks/components/tasks-list/tasks-list.component.ts
+++ src/app/tasks/components/tasks-list/tasks-list.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Task } from './../../model/task';
+import { Observable } from 'rxjs/Observable';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-tasks-list',
-  templateUrl: './tasks-list.component.html',
-  styleUrls: ['./tasks-list.component.scss']
+  templateUrl: './tasks-list.component.html'
 })
-export class TasksListComponent implements OnInit {
+export class TasksListComponent {
 
-  constructor() { }
+  // tslint:disable-next-line:no-input-rename
+  @Input('tasks') tasks$: Observable<Task[]>;
+  @Output() remove: EventEmitter<any> = new EventEmitter(false);
+  @Output() edit: EventEmitter<any> = new EventEmitter(false);
 
-  ngOnInit() {
+  onRemove(task) {
+    this.remove.emit(task);
   }
 
+  onUpdate(task, changes) {
+    this.edit.emit({task: task, updates: changes});
+  }
 }
