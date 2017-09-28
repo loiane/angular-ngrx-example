cd9ad083ca88aa391b9f5e51ea881db4e978cca1
loiane <loianeg@gmail.com>
2017-09-28 20:34:18 -0300

    removed unused imports

--- src/app/tasks/store/task.effects.ts
+++ src/app/tasks/store/task.effects.ts
@@ -7,7 +7,6 @@ import 'rxjs/add/operator/switchMap';
 
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 import { TaskService } from './../services/task.service';
