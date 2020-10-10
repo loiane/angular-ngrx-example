443689d4f7eae989e80d296c05198a008f4651af
Loiane Groner <>
2020-10-09 22:11:10 -0400

    added product list

---
+++ src/app/products/components/product-list/product-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductListComponent } from './product-list.component';
+
+describe('ProductListComponent', () => {
+  let component: ProductListComponent;
+  let fixture: ComponentFixture<ProductListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,9 +7,10 @@ import { ProductsComponent } from './container/products/products.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductEffects } from './store/product.effects';
 import * as fromProduct from './store/product.reducer';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductListComponent],
   imports: [
     CommonModule,
     ProductsRoutingModule,
